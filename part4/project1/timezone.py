f5d2f088fdf882f94bdcb03d7c32e1f2ccca459b
oliver.naaris <5196966+olivernaaris@users.noreply.github.com>
2022-10-04 23:18:27 +0300

    Transactions

--- part4/project1/timezone.py
+++ part4/project1/timezone.py
@@ -1,6 +1,10 @@
 import itertools
 import numbers
 from datetime import timedelta
+from datetime import datetime
+from collections import namedtuple
+
+Confirmation = namedtuple('Confirmation', 'account_number, transaction_code, transaction_id, time_utc, time')
 
 
 class TimeZone:
@@ -53,12 +57,31 @@ class TimeZone:
 
 class Account:
     transaction_counter = itertools.count(100)
+    _interest_rate = 0.5  # percentage
 
-    def __init__(self, account_number, first_name, last_name):
+    _transaction_codes = {
+        'deposit': 'D',
+        'withdraw': 'W',
+        'interest': 'I',
+        'rejected': 'X'
+    }
+
+    def __init__(self, account_number, first_name, last_name,
+                 timezone=None, initial_balance=0):
         # in practice we probably would want to add checks to make sure these values are valid / non-empty
         self._account_number = account_number
         self.first_name = first_name
         self.last_name = last_name
+
+        if timezone is None:
+            timezone = TimeZone('UTC', 0, 0)
+        self.timezone = timezone
+
+        self._balance = float(initial_balance)
+
+    @property
+    def balance(self):
+        return self._balance
 
     @property
     def account_number(self):
@@ -85,15 +108,90 @@ class Account:
     def full_name(self):
         return f'{self.first_name} {self.last_name}'
 
+    @property
+    def timezone(self):
+        return self._timezone
+
+    @timezone.setter
+    def timezone(self, value):
+        if not isinstance(value, TimeZone):
+            raise ValueError('Time Zone must be a valid TimeZone object.')
+        self._timezone = value
+
+    @classmethod
+    def get_interest_rate(cls):
+        return cls._interest_rate
+
+    @classmethod
+    def set_interest_rate(cls, value):
+        if not isinstance(value, numbers.Real):
+            raise ValueError('Interest rate must be a real number.')
+        if value < 0:
+            raise ValueError('Interest rate cannot be negative.')
+        cls._interest_rate = value
+
     def validate_and_set_name(self, property_name, value, field_title):
         if value is None or len(str(value).strip()) == 0:
             raise ValueError(f'{field_title} cannot be empty.')
         setattr(self, property_name, value)
+
+    def generate_confirmation_code(self, transaction_code):
+        # main difficulty here is to generate the current time in UTC using this formatting:
+        # YYYYMMDDHHMMSS
+        dt_str = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+        return f'{transaction_code}-{self.account_number}-{dt_str}-{next(Account.transaction_counter)}'
+
+    @staticmethod
+    def parse_confirmation_code(confirmation_code, preferred_time_zone=None):
+        # dummy-A100-20190325224918-101
+        parts = confirmation_code.split('-')
+        if len(parts) != 4:
+            # really simplistic validation here - would need something better
+            raise ValueError('Invalid confirmation code')
+
+        # unpack into separate variables
+        transaction_code, account_number, raw_dt_utc, transaction_id = parts
+
+        # need to convert raw_dt_utc into a proper datetime object
+        try:
+            dt_utc = datetime.strptime(raw_dt_utc, '%Y%m%d%H%M%S')
+        except ValueError as ex:
+            # again, probably need better error handling here
+            raise ValueError('Invalid transaction datetime') from ex
+
+        if preferred_time_zone is None:
+            preferred_time_zone = TimeZone('UTC', 0, 0)
+
+        if not isinstance(preferred_time_zone, TimeZone):
+            raise ValueError('Invalid TimeZone specified.')
+
+        dt_preferred = dt_utc + preferred_time_zone.offset
+        dt_preferred_str = f"{dt_preferred.strftime('%Y-%m-%d %H:%M:%S')} ({preferred_time_zone.name})"
+
+        return Confirmation(account_number, transaction_code, transaction_id, dt_utc.isoformat(), dt_preferred_str)
+
+    def make_transaction(self):
+        return self.generate_confirmation_code('dummy')
+
 
 try:
     a = Account('12345', 'John', '')
 except ValueError as ex:
     print(ex)
 
-a = Account('12345', 'Alex', 'Martelli')
-print(a.first_name, a.last_name, a.full_name)
+a1 = Account('12345', 'Alex', 'Martelli', initial_balance=100)
+a2 = Account('12345', 'John', 'Cleese', initial_balance=200)
+print(a1.first_name, a1.last_name, a1.full_name, a1.timezone.name, a1.balance)
+print(a2.first_name, a2.last_name, a2.full_name, a2.timezone.name, a2.balance)
+
+a1 = Account('A100', 'John', 'Cleese', initial_balance=100)
+a2 = Account('B100', 'Alex', 'Martelli', initial_balance=200)
+a = Account('A100', 'John', 'Cleese', initial_balance=100)
+conf_code = a.make_transaction()
+print(conf_code)
+print(Account.parse_confirmation_code(conf_code))
+print(Account.parse_confirmation_code(conf_code, TimeZone('MST', -7, 0)))
+try:
+    Account.parse_confirmation_code('X-A100-asdasd-123')
+except ValueError as ex:
+    print(ex)
